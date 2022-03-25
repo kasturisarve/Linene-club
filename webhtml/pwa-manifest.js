a2a9f9fed5945e960576878b70417ebd61b08d71
kasturi <sarvekasturi@gmail.com>
2022-03-25 19:44:54 +0530

    initial commit

---
+++ webhtml/pwa-manifest.js
@@ -0,0 +1,23 @@
+{			  
+			"name": "Futurism Technologies",
+			"short_name": "Futurism Technologies",
+			"description": "",
+			"icons": [
+			    {
+			      "src": "https://www.futurismtechnologies.com/wp-content/uploads/2019/01/logo.png",
+			      "sizes": "192x192",
+			      "type": "image\/png"
+			    },
+			    {
+			      "src": "https://www.futurismtechnologies.com/wp-content/uploads/2019/01/favicon-1.png",
+			      "sizes": "512x512",
+			      "type": "image\/png"
+			    }
+			],
+			"background_color": "#D5E0EB",
+			"theme_color": "#D5E0EB",
+			"display": "standalone",
+			"orientation": "portrait",
+			"start_url": "https://www.futurismtechnologies.com/",
+			"scope": "https://www.futurismtechnologies.com/"
+		}
